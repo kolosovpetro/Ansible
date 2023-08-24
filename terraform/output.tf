233ac5d79d6b7b7ec3beea627038236cad409873
kolosovpetro <kolosovp94@gmail.com>
2023-08-24 13:23:35 +0200

    output username | cleanups

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,7 @@ output "web_server_public_ip" {
 output "db_server_public_ip" {
   value = module.db_server.public_ip_address
 }
+
+output "os_user_name" {
+  value = var.os_profile_admin_username
+}
